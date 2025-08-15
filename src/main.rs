944c2cd468398b216ecb7c6269f639082a2018ad
bomky <bom_ky@outlook.com>
2025-08-15 15:05:24 +0800

    更新

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,64 @@
 use std::{
     collections::HashMap,
-    fs,
     sync::{Arc, Mutex},
     time::Duration,
 };
 mod config;
-use actix_web::{
-    App, HttpRequest, HttpResponse, HttpServer, Responder, cookie::time::format_description::parse,
-    get, web,
-};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
 use config::{APP_CONFIG, PLAYER_HTML};
+use moka::future::Cache;
 use reqwest;
-use scraper::{Html, Selector, selector};
+use scraper::{Html, Selector};
 use serde_json::Value;
-#[get("/greet/{name}")]
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    HttpResponse::Ok().body(format!("Hello, {}!", name))
-}
+const ONE_WEEK_IN_SECONDS: u64 = 60 * 60 * 24 * 7;
+
 #[get("/hl/{id}")]
 async fn hl(
     config: web::Data<Arc<Mutex<HashMap<String, String>>>>,
+    cache: web::Data<Cache<String, String>>,
     id: web::Path<String>,
 ) -> impl Responder {
-    let config = config.lock().unwrap();
-    let heiliao = config.get("heiliao").unwrap();
-    let url = format!("{}//archives/{}/", heiliao, id);
-    let client = reqwest::Client::new();
-    let response = client.get(&url).send().await;
-    match response {
-        Ok(resp) => {
-            if resp.status().is_success() {
-                let html = resp.text().await.unwrap();
-                let document = Html::parse_document(&html);
-                let selector = Selector::parse(".dplayer").unwrap();
-                let video = document.select(&selector).next().unwrap();
-                let video_config = video.value().attr("config").unwrap_or("");
-                //序列化
-                let video_config: Value = serde_json::from_str(video_config).unwrap();
-                let url = video_config["video"]["url"].as_str().unwrap();
-                println!("Video URL: {}", url);
-                let player_html = PLAYER_HTML.clone();
-                let player_html = player_html.replace("#####", url);
-                HttpResponse::Ok()
-                    .content_type("text/html")
-                    .body(player_html)
-            } else {
-                HttpResponse::BadRequest().body("Failed to fetch data")
+    let key = format!("hl_{}", id);
+    let video_url = cache.get(&key).await;
+    match video_url {
+        Some(url) => {
+            let player_html = PLAYER_HTML.clone();
+            let player_html = player_html.replace("#####", &url);
+            HttpResponse::Ok()
+                .content_type("text/html")
+                .body(player_html)
+        }
+        None => {
+            let config = config.lock().unwrap();
+            let heiliao = config.get("heiliao").unwrap();
+            let url = format!("{}//archives/{}/", heiliao, id);
+            let client = reqwest::Client::new();
+            let response = client.get(&url).send().await;
+            match response {
+                Ok(resp) => {
+                    if resp.status().is_success() {
+                        let html = resp.text().await.unwrap();
+                        let document = Html::parse_document(&html);
+                        let selector = Selector::parse(".dplayer").unwrap();
+                        let video = document.select(&selector).next().unwrap();
+                        let video_config = video.value().attr("config").unwrap_or("");
+                        //序列化
+                        let video_config: Value = serde_json::from_str(video_config).unwrap();
+                        let url = video_config["video"]["url"].as_str().unwrap();
+                        cache.insert(key, url.to_string()).await;
+                        println!("Video URL: {}", url);
+                        let player_html = PLAYER_HTML.clone();
+                        let player_html = player_html.replace("#####", url);
+                        HttpResponse::Ok()
+                            .content_type("text/html")
+                            .body(player_html)
+                    } else {
+                        HttpResponse::BadRequest().body("Failed to fetch data")
+                    }
+                }
+                Err(_) => HttpResponse::InternalServerError().body("Internal Server Error"),
             }
         }
-        Err(_) => HttpResponse::InternalServerError().body("Internal Server Error"),
     }
 }
 
@@ -65,9 +74,13 @@ async fn main() -> std::io::Result<()> {
             APP_CONFIG.config.meiridasai.clone(),
         );
     }
+    let cache: Cache<String, String> = Cache::builder()
+        .time_to_live(Duration::from_secs(ONE_WEEK_IN_SECONDS))
+        .build();
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(config.clone()))
+            .app_data(web::Data::new(cache.clone()))
             .service(hl)
     })
     .bind(("0.0.0.0", 17618))?
